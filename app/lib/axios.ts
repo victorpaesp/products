7461cf0b6d349baa451fbc48d4024d5d989e1ec4
Eduardo C <eduardocslv@gmail.com>
2025-05-01 22:31:19 -0300

    Update Axios base URL to use HTTPS for secure API communication

--- app/lib/axios.ts
+++ app/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://159.223.153.148/api",
+  baseURL: "https://159.223.153.148/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
