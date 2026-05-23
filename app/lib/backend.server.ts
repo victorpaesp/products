95ffe7fb003f5b64f6db302b7c958be8c8b75974
Victor <victorpaesp@gmail.com>
2026-05-23 10:18:53 -0300

    fix: adjust hmg api url

--- app/lib/backend.server.ts
+++ app/lib/backend.server.ts
@@ -2,7 +2,7 @@ import type { ApiResponse, LoginResponse, RegisterData, User } from "~/types";
 import type { RequestOptions } from "~/types/server";
 
 const API_BASE_URL =
-  process.env.BACKEND_API_URL ?? "https://hmg-searchm.on-forge.com/";
+  process.env.BACKEND_API_URL ?? "https://hmg-searchm.on-forge.com/api";
 
 export class BackendApiError extends Error {
   status: number;
